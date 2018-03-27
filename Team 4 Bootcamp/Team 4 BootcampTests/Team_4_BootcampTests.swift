4c9930dfda43c36990a617a1f99a69e79cf3e15f
Alessandra <alessandra.pereira@concrete.com.br>
2018-03-27 15:01:30 -0300

    First commit

---
+++ Team 4 Bootcamp/Team 4 BootcampTests/Team_4_BootcampTests.swift
@@ -0,0 +1,36 @@
+//
+//  Team_4_BootcampTests.swift
+//  Team 4 BootcampTests
+//
+//  Created by alessandra.l.pereira on 27/03/18.
+//  Copyright © 2018 alessandra.l.pereira. All rights reserved.
+//
+
+import XCTest
+@testable import Team_4_Bootcamp
+
+class Team_4_BootcampTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

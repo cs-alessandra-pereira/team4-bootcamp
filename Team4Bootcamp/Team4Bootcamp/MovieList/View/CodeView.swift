057015eab1c22741a8b3d7b47840cf0e64e26eb3
Alessandra <alessandra.pereira@concrete.com.br>
2018-04-02 15:39:41 -0300

    Adds SnapKit to project, creates a CodeView file and adds a new file for a better implementation of the MovieCollectionViewCell

---
+++ Team4Bootcamp/Team4Bootcamp/MovieList/View/CodeView.swift
@@ -0,0 +1,9 @@
+//
+//  CodeView.swift
+//  Team4Bootcamp
+//
+//  Created by alessandra.l.pereira on 02/04/18.
+//  Copyright © 2018 alessandra.l.pereira. All rights reserved.
+//
+
+import Foundation

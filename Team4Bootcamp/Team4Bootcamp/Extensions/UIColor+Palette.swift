156cd5d7affe81a9e693b38c0465e707603ad549
Alessandra <alessandra.pereira@concrete.com.br>
2018-04-11 13:54:20 -0300

    Adds missing extensions files from git into project

---
+++ Team4Bootcamp/Team4Bootcamp/Extensions/UIColor+Palette.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Palette.swift
+//  Team4Bootcamp
+//
+//  Created by alessandra.l.pereira on 04/04/18.
+//  Copyright © 2018 alessandra.l.pereira. All rights reserved.
+//
+import UIKit
+
+extension UIColor {
+    static let primaryColor = UIColor(named: "PrimaryColor")
+    static let secondaryColor = UIColor(named: "SecondaryColor")
+    static let terciaryColor = UIColor(named: "TerciaryColor")
+    static let accentColor = UIColor(named: "AccentColor")
+}

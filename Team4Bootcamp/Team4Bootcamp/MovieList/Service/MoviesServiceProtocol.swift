71063f610da992ad074baa8d700fd90fccf38601
cs-antonio-rodrigues <antonio.rodrigues@concrete.com.br>
2018-04-11 20:35:43 -0300

    Removes comments

--- Team4Bootcamp/Team4Bootcamp/MovieList/Service/MoviesServiceProtocol.swift
+++ Team4Bootcamp/Team4Bootcamp/MovieList/Service/MoviesServiceProtocol.swift
@@ -9,9 +9,7 @@
 import Foundation
 
 protocol MoviesServiceProtocol {
-    // Como eu sei que foi um erro ?
     func fetchMovies(callback: @escaping ([Movie]) -> Void)
-    // Agora eu tenho como tomar alguma acao caso venha um erro
     func fetchGenres(callback: @escaping ([GenreId: GenreName]) -> Void)
 }
 

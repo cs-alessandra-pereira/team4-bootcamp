6a0539ffb80c5220ba898dd7621f1f2b31c14233
Alessandra <alessandra.pereira@concrete.com.br>
2018-04-11 14:34:43 -0300

    Updates tests target

---
+++ Team4Bootcamp/Team4BootcampTests/MovieList/Controller/MovieListViewControllerSpec.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListViewControllerSpec.swift
+//  Team4BootcampTests
+//
+//  Created by alessandra.l.pereira on 11/04/18.
+//  Copyright © 2018 alessandra.l.pereira. All rights reserved.
+//
+
+import Foundation

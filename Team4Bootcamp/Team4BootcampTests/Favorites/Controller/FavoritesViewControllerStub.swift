493c0759e6e1b767f0ff864f5c4699ec9e30c833
cs-antonio-rodrigues <antonio.rodrigues@concrete.com.br>
2018-04-26 20:06:03 -0300

    Adds initial testing for FavoritesViewController

---
+++ Team4Bootcamp/Team4BootcampTests/Favorites/Controller/FavoritesViewControllerStub.swift
@@ -0,0 +1,48 @@
+//
+//  FavoritesViewController.swift
+//  Team4BootcampTests
+//
+//  Created by a.portela.rodrigues on 26/04/18.
+//  Copyright © 2018 alessandra.l.pereira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+@testable import Team4Bootcamp
+
+class FavoritesViewControllerStub: FavoritesViewController {
+    
+    var setupDataSourceWasCalled = false
+    var setupDelegateWasCalled = false
+    var setupSearchBarWasCalled = false
+    var setupNSFetchedWasCalled = false
+    
+    override func setupDataSource() {
+        setupDataSourceWasCalled = true
+    }
+    
+    override func setupDelegate() {
+        setupDelegateWasCalled = true
+    }
+    
+    override func setupSearchBar() {
+        setupSearchBarWasCalled = true
+    }
+    
+    override func setupNSFetched() {
+        setupNSFetchedWasCalled = true
+    }
+    
+    static func initFromStoryboard() -> FavoritesViewController {
+        
+        let storyboard = UIStoryboard(name: "Main",
+                                      bundle: Bundle(for: FavoritesViewController.self))
+        
+        let className = String.init(describing: FavoritesViewController.self)
+        
+        guard let viewController = storyboard.instantiateViewController(withIdentifier: className) as? FavoritesViewController else {
+            fatalError("Could not load controller called \(className)")
+        }
+        return viewController
+    }
+}

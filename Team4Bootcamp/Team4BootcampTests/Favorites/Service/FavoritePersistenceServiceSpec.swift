85b70913827bcfb564e603ae3bdcab30105ba598
cs-alessandra-pereira <alessandra.pereira@concrete.com.br>
2018-05-02 18:24:57 -0300

    Removes unnecessary comments

--- Team4Bootcamp/Team4BootcampTests/Favorites/Service/FavoritePersistenceServiceSpec.swift
+++ Team4Bootcamp/Team4BootcampTests/Favorites/Service/FavoritePersistenceServiceSpec.swift
@@ -16,8 +16,7 @@ class FavoritePersistenceServiceSpec: QuickSpec {
     
     lazy var mockPersistantContainer: NSPersistentContainer = {
         
-        let container = NSPersistentContainer(name: PersistenceConstants.persistenceContainerName, managedObjectModel: self.managedObjectModel)
-        
+        let container = NSPersistentContainer(name: PersistenceConstants.persistenceContainerName)
         let description = NSPersistentStoreDescription()
         description.type = NSInMemoryStoreType
         description.shouldAddStoreAsynchronously = false
@@ -33,30 +32,16 @@ class FavoritePersistenceServiceSpec: QuickSpec {
         return container
     }()
     
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: type(of: self))] )!
-        return managedObjectModel
-    }()
-    
     override func spec() {
         
         
         func initStubs() {
             let movie1 = Movie(id: 337167, title: "Fifty Shades Freed", releaseDate: Date(timeIntervalSince1970: 0), genres: [Genre(id: 18), Genre(id: 10749)], overview: "Believing they have left behind...", posterPath: "/jjPJ4s3DWZZvI4vw8Xfi4Vqa1Q8.jpg", persisted: false)
             
-            let movie2 = Movie(id: 269149, title: "Zootopia", releaseDate: Date(timeIntervalSince1970: 0), genres: [Genre(id: 16), Genre(id: 12), Genre(id: 10751),Genre(id: 35)], overview: "Determined to prove herself...", posterPath: "/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg", persisted: false)
+            let movie2 = Movie(id: 269149, title: "Zootopia", releaseDate: Date(timeIntervalSince1970: 0), genres: [Genre(id: 16), Genre(id: 12), Genre(id: 10751), Genre(id: 35)], overview: "Determined to prove herself...", posterPath: "/sM33SANp9z6rXW8Itn7NnG1GOEs.jpg", persisted: false)
             
             _ = MovieDAO.addMovie(movie: movie1, context: mockPersistantContainer.viewContext)
             _ = MovieDAO.addMovie(movie: movie2, context: mockPersistantContainer.viewContext)
-        }
-        
-        func flushData() {
-            let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest<NSFetchRequestResult>(entityName: "MovieDAO")
-            let objs = try! self.mockPersistantContainer.viewContext.fetch(fetchRequest) //swiftlint:disable:this force_try
-            for case let obj as NSManagedObject in objs {
-                mockPersistantContainer.viewContext.delete(obj)
-            }
-            try? mockPersistantContainer.viewContext.save() ///swiftlint:disable:this force_try
         }
         
         describe("FavoritePersistenceService") {
@@ -69,10 +54,6 @@ class FavoritePersistenceServiceSpec: QuickSpec {
                 beforeEach {
                     sut = FavoritePersistenceService()
                     initStubs()
-                }
-                
-                afterEach {
-                    //flushData()
                 }
                 
                 it("Database should include 2 items") {
@@ -92,4 +73,3 @@ class FavoritePersistenceServiceSpec: QuickSpec {
         }
     }
 }
-

66ef40df450761b1fb6319bb1fa0bcffff7960c7
cs-antonio-rodrigues <antonio.rodrigues@concrete.com.br>
2018-05-24 16:59:58 -0300

    Adds relationship between movie and genre on database

--- Team4Bootcamp/Team4Bootcamp/Persistence/GenreDAO+CoreDataProperties.swift
+++ Team4Bootcamp/Team4Bootcamp/Persistence/GenreDAO+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  GenreDAO+CoreDataProperties.swift
 //  Team4Bootcamp
 //
-//  Created by alessandra.l.pereira on 17/05/18.
+//  Created by a.portela.rodrigues on 24/05/18.
 //  Copyright © 2018 alessandra.l.pereira. All rights reserved.
 //
 //
@@ -19,23 +19,6 @@ extension GenreDAO {
 
     @NSManaged public var id: Int32
     @NSManaged public var name: String
-    @NSManaged public var movies: NSSet?
-
-}
-
-// MARK: Generated accessors for movies
-extension GenreDAO {
-
-    @objc(addMoviesObject:)
-    @NSManaged public func addToMovies(_ value: MovieDAO)
-
-    @objc(removeMoviesObject:)
-    @NSManaged public func removeFromMovies(_ value: MovieDAO)
-
-    @objc(addMovies:)
-    @NSManaged public func addToMovies(_ values: NSSet)
-
-    @objc(removeMovies:)
-    @NSManaged public func removeFromMovies(_ values: NSSet)
+    @NSManaged public var movies: MovieDAO?
 
 }

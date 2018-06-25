35ccab786c43dcaadd4265b4eae96b247b43ca3c
cs-alessandra-pereira <alessandra.pereira@concrete.com.br>
2018-06-25 16:24:17 -0300

    Updates movieList cells size

--- Team4Bootcamp/Team4Bootcamp/MovieList/View/MovieCollectionView.swift
+++ Team4Bootcamp/Team4Bootcamp/MovieList/View/MovieCollectionView.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SnapKit
 
 class MovieCollectionView: UICollectionView {
 
@@ -22,10 +23,11 @@ class MovieCollectionView: UICollectionView {
     }
     
     func setupView() {
+        let screenSize = UIScreen.main.bounds
         let insets = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.sectionInset = insets
-        layout.itemSize = CGSize(width: 180, height: 220)
+        layout.estimatedItemSize = CGSize(width: screenSize.width*3/7, height: 220)
         self.collectionViewLayout = layout
         self.backgroundColor = UIColor.white
     }
---
+++ Team4Bootcamp/Team4BootcampTests/MovieList/View/MovieCollectionViewSpec.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCollectioViewSpec.swift
+//  Team4BootcampTests
+//
+//  Created by alessandra.l.pereira on 25/06/18.
+//  Copyright © 2018 alessandra.l.pereira. All rights reserved.
+//
+
+import Quick
+import Nimble
+import Nimble_Snapshots
+
+@testable import Team4Bootcamp
+
+class MovieCollectionViewSpec: QuickSpec {
+    override func spec() {
+        describe("MovieCollectionView UI") {
+            it("should have the expected look and feel") {
+//                let movie = Movie(id: 01, title: "Zootopia", releaseDate: nil, genresIds: [12], overview: "Blablablablabla blablablabla blabla blablablabla blablabla", posterPath: "/eKi8dIrr8voobb", persisted: false)
+//                let movies = [movie, movie, movie, movie, movie, movie]
+//                let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+//                let frame = CGRect(x: 0, y: 0, width: 414, height: 736)
+//                let view = MovieListViewController(nibName: nil, bundle: nil)
+//                expect(view) == recordSnapshot("MovieCollectionView")
+            }
+        }
+    }
+}

1e63e96d793ca7d03a183996c536aa65e66628dd
cs-alessandra-pereira <alessandra.pereira@concrete.com.br>
2018-06-25 20:33:40 -0300

    Updates MovieDetail UI test after code refactoring

--- Team4Bootcamp/Team4BootcampTests/MovieDetails/View/MovieDetailsViewSpec.swift
+++ Team4Bootcamp/Team4BootcampTests/MovieDetails/View/MovieDetailsViewSpec.swift
@@ -15,6 +15,12 @@ import Nimble_Snapshots
 class MovieDetailsViewSpec: QuickSpec {
     override func spec() {
         describe("MovieDetailsView UI") {
+            
+            beforeEach {
+                let movieListViewController = MovieListViewController()
+                movieListViewController.fetchGenres()
+            }
+
             it("should have the expected look and feel") {
                 let movie = Movie(id: 01, title: "Zootopia", releaseDate: nil, genresIds: [12], overview: "Blablablablabla blablablabla blabla blablablabla blablabla", posterPath: "/eKi8dIrr8voobb", persisted: false)
                 let frame = CGRect(x: 0, y: 0, width: 414, height: 736)
@@ -22,7 +28,7 @@ class MovieDetailsViewSpec: QuickSpec {
                 let datasource = MovieDetailsDatasource(tableView: view.tableView, movie: movie)
                 view.tableView.dataSource = datasource
                 view.posterImage.backgroundColor = .black
-                expect(view) == recordSnapshot("MovieDetailsView")
+                expect(view) == snapshot("MovieDetailsView")
             }
         }
     }
